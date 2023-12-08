7fbde7c772cad2417a7808056f54e5a4fabcaf54
rathod-tirth <tirthrathod6@gmail.com>
2023-12-08 20:19:01 +0530

    update typnos

--- Task/08-list-duplicate-remove.py
+++ Task/08-list-duplicate-remove.py
@@ -1,4 +1,3 @@
-
 # removes duplicate numbers from the list
 
 li=[23,45,12,19,23,12,15,10,45,65,19,23,15,12]
@@ -8,7 +7,7 @@ for i in li:
     if(i not in newLi):
         newLi.append(i)
 
-print("Original List",li)
+print("Original List:",li)
 print("New List:",newLi)
 
 
