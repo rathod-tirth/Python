877ba04ef8760944034c235bf2f51b15583a9362
rathod-tirth <tirthrathod6@gmail.com>
2023-12-08 17:02:24 +0530

    change folder

---
+++ Task/task-dict.py
@@ -0,0 +1,13 @@
+# task
+
+student={}
+
+name=input("Enter name: ")
+roll=int(input("Enter roll: "))
+marks=int(input("Enter Marks: "))
+
+student['name']=name
+student['roll']=roll
+student['marks']=marks
+
+print(student)
