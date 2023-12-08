76e00bf184ed2b78bc4dde902c1a988111c5d49a
rathod-tirth <tirthrathod6@gmail.com>
2023-12-08 20:10:39 +0530

    update swaping

--- Task/07-list-sort.py
+++ Task/07-list-sort.py
@@ -5,9 +5,7 @@ li=[21,56,47,8,23,42,10,46,68,92,37]
 for i in range(len(li)):
     for j in range(i,len(li)):
         if(li[i]>li[j]):
-            li[j]=li[i]+li[j]
-            li[i]=li[j]-li[i]
-            li[j]=li[j]-li[i]
+            li[i],li[j]=li[j],li[i]
 print(li)
         
         
@@ -16,7 +14,5 @@ print(li)
 for i in range(len(li)):
     for j in range(i,len(li)):
         if(li[i]<li[j]):
-            li[j]=li[i]+li[j]
-            li[i]=li[j]-li[i]
-            li[j]=li[j]-li[i]
+            li[i],li[j]=li[j],li[i]
 print(li)
