bf55710323946559494fa0a7f065204761a89737
rathod-tirth <tirthrathod6@gmail.com>
2023-12-17 20:17:35 +0530

    add dict nested

---
+++ Topics/21-dict2.py
@@ -0,0 +1,13 @@
+# tuple and list in dict values
+
+di1={"names":["Abc","Xyz","Mno","Pqr"],
+     "hobby":("Painting","Gaming","Biking"),}
+
+print("one of the hobbies:",di1["hobby"][1])
+
+# nested dict
+
+di2={"a1":{"name":"Abc","email":"abc@gmail.com"},
+     "a2":{"name":"Xyz","email":"xyz@gmail.com"}}
+
+print(di2)
