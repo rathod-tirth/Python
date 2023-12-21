245356ed08c1c600f6365509ebf3738bfc4fdc76
rathod-tirth <tirthrathod6@gmail.com>
2023-12-21 22:48:31 +0530

    update optimize class and increase code readablity

--- Task/19-oop-pizza-menu.py
+++ Task/19-oop-pizza-menu.py
@@ -1,6 +1,7 @@
 # Pizza and Pasta Menu with OOP
 
-class Menu():
+# contains all the products and pricing
+class Product():
    def __init__(self):
       # menu pricing
       self.pizza=100
@@ -21,50 +22,95 @@ class Menu():
       self.softdrink=0
       self.bruschetta=0
       self.ice_cream=0
+      
+   def menu(self):
+      print(f"""\n====================== Menu ======================
+         
+ 1 Large Pizza = ₹{self.pizza} 
+
+ 2 Large Pizzas = ₹{self.pizza*2}
+
+ 3 Large Pizzas = ₹{self.pizza*3}
+
+ ***Buy 4 or more Pizzas and get 1.5l of Soft Drink free***
+ 
+--------------------------------
+
+ 1 large Pasta = ₹{self.pasta}
+
+ 2 Large Pastas = ₹{self.pasta*2}
+
+ 3 Large Pastas = ₹{self.pasta*3}
+
+ ***Buy 4 or more Pastas and get 2 Bruschetta free.***
+
+--------------------------------
+
+ ***Buy 4 or more Pizzas and Pastas and get 2 Chocco Brownies Ice Cream free.***
+ 
+--------------------------------\n""")
+
+# contains all the calculation and billing data
+class Bill(Product):
+   def __init__(self):
+      self.total=0
+      
+      # pizza
+      self.pizzaNum=0
+      self.pastaAmount=0
+      
+      # pasta
+      self.pastaNum=0
+      self.pastaAmount=0
    
    # order placing function
    def order(self):
-      total=0
+      self.total=0
       
-      # pizza order
-      pizzaOrder=int(input("Enter no of Pizza you want : "))
-      pizzaAmount=self.pizza*pizzaOrder
-      total+=pizzaAmount
-      
-      self.pizzaSold+=pizzaOrder
-      self.pizzaTotal+=pizzaAmount
-      
-      print(f"\nYour Pizza Order Amount is : ₹{pizzaAmount}")
-      
-      # pizza special offer
-      if pizzaOrder>3:
-         self.softdrink+=1
-         print("\n *** Congratulations !! 1.5lt Softdrink free *** ")
-      
-      # pasta order
-      pastaOrder=int(input("\nEnter no of Pasta you want : "))
-      pastaAmount=self.pasta*pastaOrder
-      total+=pastaAmount
-      
-      self.pastaSold+=pastaOrder
-      self.pastaTotal+=pastaAmount
-      
-      print(f"\nYour Pasta Order Amount is : ₹{pastaAmount}")
-      
-      # pasta special offer
-      if pastaOrder>3:
-         self.bruschetta+=1
-         print("\n *** Congratulations !! Get 2 Bruschetta free ***  ")
-      
-      # combo special offer
-      if pizzaOrder>3 and pastaOrder>3:
-         self.ice_cream+=2
-         print("\n *** Congratulations !! Get 2 Chocco Brownies Ice Cream free ***")
+      self.orderPizza()
+      self.orderPasta()
          
+      total=self.pizzaAmount+self.pastaAmount
       print(f"\nYour Total Order is: ₹{total}")
       self.allTotal+=total
       
       return self.allTotal
+   
+   def orderPizza(self):
+      # pizza order
+      self.pizzaNum=int(input("Enter no of Pizza you want : "))
+      self.pizzaAmount=self.pizza*self.pizzaNum
+      
+      self.pizzaSold+=self.pizzaNum
+      self.pizzaTotal+=self.pizzaAmount
+      
+      print(f"\nYour Pizza Order Amount is : ₹{self.pizzaAmount}")
+      
+      # pizza special offer
+      if self.pizzaNum>3:
+         self.softdrink+=1
+         print("\n *** Congratulations !! 1.5lt Softdrink free *** ")
+         
+   def orderPasta(self):
+       # pasta order
+      self.pastaNum=int(input("\nEnter no of Pasta you want : "))
+      self.pastaAmount=self.pasta*self.pastaNum
+      
+      self.pastaSold+=self.pastaNum
+      self.pastaTotal+=self.pastaAmount
+      
+      print(f"\nYour Pasta Order Amount is : ₹{self.pastaAmount}")
+      
+      # pasta special offer
+      if self.pastaNum>3:
+         self.bruschetta+=1
+         print("\n *** Congratulations !! Get 2 Bruschetta free ***  ")
+   
+   # combo special offer
+   def special_offer(self):
+      if self.pizzaOrder>3 and self.pastaOrder>3:
+         self.ice_cream+=2
+         print("\n *** Congratulations !! Get 2 Chocco Brownies Ice Cream free ***")
    
    # Shift summary with earnings and sellings
    def summary(self):
@@ -89,7 +135,7 @@ class Menu():
  Bye Bye !!!""")
          
 # object
-menu=Menu()
+bill=Bill()
 
 # loop depandant
 status=True
@@ -107,38 +153,14 @@ if choice!=1:
 # loop
 while status:
    # menu printed to user
-   print(f"""\n====================== Menu ======================
-         
- 1 Large Pizza = ₹{menu.pizza} 
-
- 2 Large Pizzas = ₹{menu.pizza*2}
-
- 3 Large Pizzas = ₹{menu.pizza*3}
-
- ***Buy 4 or more Pizzas and get 1.5l of Soft Drink free***
- 
---------------------------------
-
- 1 large Pasta = ₹{menu.pasta}
-
- 2 Large Pastas = ₹{menu.pasta*2}
-
- 3 Large Pastas = ₹{menu.pasta*3}
-
- ***Buy 4 or more Pastas and get 2 Bruschetta free.***
-
---------------------------------
-
- ***Buy 4 or more Pizzas and Pastas and get 2 Chocco Brownies Ice Cream free.***
- 
---------------------------------\n""")
+   bill.menu()
    
    # customer name
    name=input("Enter your name: ")
    print(f"\nWelcome, {name}\n")
 
    # customer totol bill
-   print(f"\n==>> Your Net Order Amount of the day is : ₹{menu.order()}")
+   print(f"\n==>> Your Net Order Amount of the day is : ₹{bill.order()}")
    
    # for more customers
    more=input("\nDo you want to enter from another customer? [y/n] : ")
@@ -147,4 +169,4 @@ while status:
       status=True
    else:
       status=False
-      menu.summary()
+      bill.summary()
