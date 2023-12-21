72d3142535072b22e321f962c4ec89cac50f9624
rathod-tirth <tirthrathod6@gmail.com>
2023-12-21 23:20:38 +0530

    fix typos

--- Task/square-root.py
+++
@@ -1,7 +0,0 @@
-# square root of number
-
-num=int(input("Enter num: "))
-
-sqr_root=lambda x: x**(1/2)
-
-print(sqr_root(num))
--- Topics/constructor.py
+++
@@ -1,7 +0,0 @@
-class Student:
-   id =10
-
-   def __init__(self):
-      print("Welcome to tops")
-
-s1= Student()
