a64c526e7cdb3be97a177c33ba62be88e4163ec9
rathod-tirth <tirthrathod6@gmail.com>
2024-01-02 17:37:12 +0530

    delete

--- 05-Packages/Package2/Module1/module1.py
+++
@@ -1,5 +0,0 @@
-def fact(x):
-   result=1
-   for i in range(1,x+1):
-      result*=i
-   return result
--- 05-Packages/Package2/display.py
+++
@@ -1,5 +0,0 @@
-from Module1 import module1
-
-num=int(input("Enter a num : "))
-
-print("Factorial of that number :", module1.fact(num))
