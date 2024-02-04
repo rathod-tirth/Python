644a17f970fe33f91b02edc81fbf287c332503e2
rathod-tirth <tirthrathod6@gmail.com>
2024-02-04 22:57:48 +0530

    add batorball logic

--- 00-Task/08-Summary (Task)/ipl2.py
+++ 00-Task/08-Summary (Task)/ipl2.py
@@ -4,17 +4,24 @@ import random
 
 class GameData:
    def __init__(self):
+      # team
       self.team=["CSK","MI","GT","RCB","SRH","KKR"]
       self.user_team=""
       self.comp_team=""
       
+      # toss
+      self.coin=["Head","Tail"]
+      self.tossresult=-1
+      self.tosswinner=""
+      
+      self.batORball=["Bat","Ball"]
+      
 class GameRandom:
-   def tossGenration(self):
-      result=random.randint(0,1)
-      return result
+   pass
 
 class GameLogic(GameData,GameRandom):
    def teamSelection(self):
+      # user team choice
       user_choice=int(input("\nEnter your choice : "))
       
       # for out of bound team selection
@@ -22,20 +29,42 @@ class GameLogic(GameData,GameRandom):
          print("\nWrong Input Pls enter again")
          user_choice=int(input("Enter your choice : "))
 
+      # assiging user and comp team
       self.user_team=self.team[user_choice-1]
       self.team.remove(self.user_team)
       self.comp_team=random.choice(self.team)
       
-   def toss(self):
+   def tossLogic(self):
+      # user toss choice
       user_toss=int(input("Enter your choice : "))
       
-      while user_toss>1:
+      while user_toss>len(self.coin) or user_toss<1:
          print("\nWrong Input Pls enter again")
          user_toss=int(input("Enter your choice : "))
 
-      result=self.tossGenration()
+      self.tossresult=random.choice(self.coin)
+      
+      if self.tossresult==self.coin[user_toss-1]:
+         self.tosswinner="User"
+      else:
+         self.tosswinner="Computer"
+   
+   def batORballLogic(self):
+      if self.tosswinner.casefold()=="user":
+         user_batORball=int(input("Enter your choice : "))
+         
+         while user_batORball>len(self.batORball):
+            print("\nWrong Input Pls enter again")
+            user_batORball=int(input("Enter your choice : "))
+            
+         return self.batORball[user_batORball-1]
+      else:
+         comp_batORball=random.choice(self.batORball)
+         return comp_batORball
+         
 
 class PrintOut(GameLogic):
+   # initial render
    def __init__(self):
       super().__init__()
       print("Team Selection".center(50)+f"\n{dash}\n")
@@ -46,7 +75,7 @@ class PrintOut(GameLogic):
       for i in self.team:
          print(f"Press {count} for : {i}")
          count+=1
-         
+   
    def teamPrint(self):
       self.teamSelection()
       
@@ -58,7 +87,18 @@ class PrintOut(GameLogic):
       print("Toss".center(50)+f"\n{dash}\n")
       print("Chose your Toss : \n")
       print("Press 1 for : Head\nPress 2 for : Tail\n")
+      self.tossLogic()
+      
+      print("\nToss :",self.tossresult)
+      print(f"{self.tosswinner} win the toss\n")
    
+   def batORballPrint(self):
+      if self.tosswinner.casefold()=="user":
+         print("Press 1 for : Bat\nPress 2 for : Ball\n")
+      
+      result=self.batORballLogic()
+      print(f"\n{self.tosswinner} choose to {result}")
+      
 # title
 dash="----------------------------------------------------"
 print("IPL".center(50)+f"\n{dash}")
@@ -66,3 +106,4 @@ print("IPL".center(50)+f"\n{dash}")
 ipl=PrintOut()
 ipl.teamPrint()
 ipl.tossPrint()
+ipl.batORballPrint()
