130382cc659897541c572da30fa208126c85b49d
rathod-tirth <tirthrathod6@gmail.com>
2024-02-05 22:11:56 +0530

    task complete

--- 00-Task/08-Summary (Task)/ipl2.py
+++ 00-Task/08-Summary (Task)/ipl2.py
@@ -15,9 +15,12 @@ class GameData:
       self.tosswinner=""
       
       self.batORball=["Bat","Ball"]
-      self.bat=""
+      self.batteam=""
+      self.ballteam=""
       
       self.score=[0,1,2,3,4,6,'W','NB','WIDE']
+      self.runs=0
+      self.innings=1
       
 class GameRandom:
    pass
@@ -57,28 +60,82 @@ class GameLogic(GameData,GameRandom):
          user_batORball=int(input("Enter your choice : "))
          result=self.batORball[user_batORball-1]
          
-         while user_batORball>len(self.batORball):
+         while user_batORball>2 or user_batORball<1:
             print("\nWrong Input Pls enter again")
             user_batORball=int(input("Enter your choice : "))
-         
-      else:
-         comp_batORball=random.choice(self.batORball)
-         result=comp_batORball
 
-      if result.lower()=='bat':
-         self.bat=self.tosswinner
-      elif self.tosswinner==self.user_team:
-         self.bat=self.comp_team
       else:
-         self.bat=self.user_team
-         
-      return result
+         result=random.choice(self.batORball)
       
+      if result.lower()=='bat':
+         self.batteam=self.user_team if self.tosswinner==self.user_team else self.comp_team
+         self.ballteam=self.comp_team if self.tosswinner==self.user_team else self.user_team
+      else:
+         self.batteam=self.comp_team if self.tosswinner==self.user_team else self.user_team
+         self.ballteam=self.user_team if self.tosswinner==self.user_team else self.comp_team
+      return result
    
    def scoreLogic(self):
-      pass
-      # f"{user_team} : {runs}/{wicket} (0.{ball})"
+      runs=0
+      wicket=0
+      ball=0
+      printball=0
+      scoreboard=""
+      freehit=False
+      
+      while ball<6:
+         ball+=1
          
+         # score
+         current_score=random.choice(self.score)
+         scoreboard+=f"{current_score} "
+         
+         if current_score=="W" and not freehit:
+            wicket=1
+         elif current_score in ["WIDE","NB"]:
+            ball-=1
+            runs+=1
+         elif isinstance(current_score,int):
+            runs+=current_score
+            
+         # ball
+         if ball%6==0:
+           printball=ball/6
+         else: 
+            printball=ball/10
+         
+         if self.innings==1:
+            print(f"\n{self.user_team} VS {self.comp_team} | {self.batteam} : {runs}/{wicket} ({printball}) | {scoreboard}")
+         else:
+            req_runs=self.runs-runs if self.runs-runs>0 else 0
+            print(f"\n{self.user_team} VS {self.comp_team} | {self.batteam} : {runs}/{wicket} ({printball}) | {req_runs} runs in {6-ball} balls | {scoreboard}")
+
+            if runs>=self.runs:
+               break
+
+         input("Press Enter to Continue...")
+         # self.runs=runs
+         
+         if current_score=="W" and not freehit:
+            break
+         if current_score=="NB":
+            freehit=True
+         else:
+            freehit=False
+      
+      if self.innings==1:
+         self.runs=runs+1
+         print(f"\n{self.ballteam} needs {self.runs} runs to win")
+         
+         self.innings=2
+         self.batteam,self.ballteam=self.ballteam,self.batteam
+         input("Press Enter to Continue...")
+         self.scoreLogic()
+      else:
+         if runs>=self.runs:
+            print(f"\n{self.batteam} won the match by 1 wicket")
+         else:
+            print(f"\n{self.ballteam} won the match by {self.runs-runs} runs")
 
 class PrintOut(GameLogic):
    # initial render
@@ -116,9 +173,8 @@ class PrintOut(GameLogic):
       result=self.batORballLogic()
       print(f"\n{self.tosswinner} choose to {result}")
       
-      # if result.lower()=="bat":
-      #    self.bat=self.tosswinner
-      # else:
+   def scorePrint(self):
+      self.scoreLogic()
          
       
 # title
@@ -129,3 +185,4 @@ ipl=PrintOut()
 ipl.teamPrint()
 ipl.tossPrint()
 ipl.batORballPrint()
+ipl.scorePrint()
